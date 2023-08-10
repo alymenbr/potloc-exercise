ea48208849ab44be7a453cc6a7f4695c7cd5d16d
Alysson Anthony Almeida Mendonca <alysson@factati.com>
2023-08-09 21:38:59 -0300

    adds animated highlights to newly created or updated inventory items

--- inventory-hub/app/models/inventory.rb
+++ inventory-hub/app/models/inventory.rb
@@ -7,14 +7,14 @@ class Inventory < ApplicationRecord
 
 
   def broadcast_created_inventory
-    broadcast_append_later_to "inventory_global", partial: 'inventories/inventory', target: 'inventory_global_id'
-    broadcast_append_later_to "inventory_store_#{self.shoe_store.id}", partial: 'inventories/inventory', target: self.shoe_store 
-    broadcast_append_later_to "inventory_model_#{self.shoe_model.id}", partial: 'inventories/inventory', target: self.shoe_model 
+    broadcast_append_later_to "inventory_global", partial: 'inventories/inventory', target: 'inventory_global_id', locals: {inventory: self, created: true, updated: false}
+    broadcast_append_later_to "inventory_store_#{self.shoe_store.id}", partial: 'inventories/inventory', target: self.shoe_store, locals: {inventory: self, created: true, updated: false}
+    broadcast_append_later_to "inventory_model_#{self.shoe_model.id}", partial: 'inventories/inventory', target: self.shoe_model, locals: {inventory: self, created: true, updated: false}
   end
 
   def broadcast_updated_inventory
-    broadcast_replace_later_to "inventory_global", partial: 'inventories/inventory'
-    broadcast_replace_later_to "inventory_store_#{self.shoe_store.id}", partial: 'inventories/inventory'
-    broadcast_replace_later_to "inventory_model_#{self.shoe_model.id}", partial: 'inventories/inventory'
+    broadcast_replace_later_to "inventory_global", partial: 'inventories/inventory', locals: {inventory: self, created: false, updated: true}
+    broadcast_replace_later_to "inventory_store_#{self.shoe_store.id}", partial: 'inventories/inventory', locals: {inventory: self, created: false, updated: true}
+    broadcast_replace_later_to "inventory_model_#{self.shoe_model.id}", partial: 'inventories/inventory', locals: {inventory: self, created: false, updated: true}
   end  
 end
